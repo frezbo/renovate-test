f77f2846c8eae9eab05062febccc64e3000a9999
Noel Georgi <git@frezbo.dev>
2025-02-05 20:06:40 +0530

    chore: test ci
    test ci.
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
 import (
+	"os"
+
 	_ "golang.org/x/oauth2"
 )
 
-func main() {}
+// DefaultBaseImage for non-scratch builds.
+// renovate: datasource=docker versioning=docker depName=alpine
+const DefaultBaseImage = "docker.io/alpine:3.21"
+
+// DefaultDirMode is UNIX file mode for mkdir.
+const DefaultDirMode os.FileMode = 0o755
+
+// DefaultPath is default value for PATH environment variable.
+const DefaultPath = "/bin:/usr/bin:/sbin:/usr/sbin"
+
+// PkgYaml is the filename of 'pkg.yaml'.
+const PkgYaml = "pkg.yaml"
+
+// VarsYaml is the filename of 'vars.yaml'.
+const VarsYaml = "vars.yaml"
+
+// Pkgfile is the filename of 'Pkgfile'.
+const Pkgfile = "Pkgfile"
+
+// StageXBusyboxImage is the image name for busybox from stageX.
+// renovate: datasource=docker versioning=docker depName=siderolabs/stagex/core-busybox
+const StageXBusyboxImage = "ghcr.io/siderolabs/stagex/core-busybox:1.36.1@sha256:c0b551b47d8f1ac2fd5f4712eafddb8717e6e563a47203e02f94f944f64c18b2"
+
+func main() {
+}

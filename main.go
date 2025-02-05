ea23f47bc47462e78ce52419a32b5ee352e158dd
Noel Georgi <git@frezbo.dev>
2025-02-05 20:48:53 +0530

    chore: test ci
    test ci.
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ const Pkgfile = "Pkgfile"
 
 // StageXBusyboxImage is the image name for busybox from stageX.
 // renovate: datasource=docker versioning=docker depName=ghcr.io/siderolabs/stagex/core-busybox
-const StageXBusyboxImage = "ghcr.io/siderolabs/stagex/core-busybox:1.36.1@sha256:c0b551b47d8f1ac2fd5f4712eafddb8717e6e563a47203e02f94f944f64c18b2"
+const StageXBusyboxImage = "ghcr.io/siderolabs/stagex/core-busybox@sha256:c0b551b47d8f1ac2fd5f4712eafddb8717e6e563a47203e02f94f944f64c18b2"
 
 func main() {
 }
